240108945ecdee8a8c7df167270ca00e8ff00b9d
ellag7121 <ellag7121@wrdsb.ca>
2020-10-08 13:34:31 +0000

    finished a6q3!

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * asks the user for 10 integers, finds the largest
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //create the array
+    int[] numbers = new int[10];
+
+    //ask for the numbers
+    System.out.println("Please enter in 10 integers to put into the array");
+    for(int i = 0; i < numbers.length; i++){
+      //store the input in the array
+      numbers[i] = input.nextInt();
+    }
     
+    //create a variable for the max number
+    int maxNum = numbers[0]; //set it to the first index of numbers, because if there is one bigger than that it will be changed in the for loop
+
+    //find the max numbers
+    for(int i = 0; i < numbers.length; i++){
+      //check if the current index is bigger that the current max
+      if(numbers[i] > maxNum){
+        //set the new max to the current number index
+        maxNum = numbers[i];
+      }
+    }
+
+    //tell the user the maximum
+      System.out.println("The largest number is " + maxNum);
   }
 }
